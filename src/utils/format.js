d1fce57e644ebe76099bd67fcc3755c1fc7881bf
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-09-04 13:51:33 +0700

    add formatIDR utility function for Indonesian Rupiah formatting

---
+++ src/utils/format.js
@@ -0,0 +1,8 @@
+// src/utils/format.js
+export const formatIDR = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0,
+    }).format(number);
+};
