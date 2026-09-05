f9dfbb8f74ebc040bfd34f23e46b4794cf56817a
Yosef Fernando Lanes <yosefflanes@gmail.com>
2026-09-05 23:09:38 +0700

    fix: add formatNumber for input form

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,8 +1,11 @@
-// src/utils/format.js
 export const formatIDR = (number) => {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
       currency: "IDR",
       minimumFractionDigits: 0,
     }).format(number);
+};
+
+export const formatNumber = (number) => {
+    return new Intl.NumberFormat("id-ID").format(number);
 };
